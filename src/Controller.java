f13b6df6d8a97e2ab737a3ffe59f2d9380df305a
Birte De Smedt <birte.desmedt@student.kuleuven.be>
2020-03-23 10:25:51 +0100

    Inlezen en uitlezen in eigen directory (git zelf) output en inputfile mee gepushed

--- src/Controller.java
+++ src/Controller.java
@@ -32,9 +32,10 @@ public class Controller
 		Beslissing b = new Beslissing(az,ra);
 		System.out.println(ra);
 		b.localSearch(in.getReservaties(), in.getZones().size(), in.getAutos());
-		Wegschrijven out = new Wegschrijven("D:/SynologyDrive/KU Leuven/Artificiële inteligentie/Wagen labo/Cambio/src/outfile.csv", b);
+		//Wegschrijven out = new Wegschrijven("D:/SynologyDrive/KU Leuven/Artificiële inteligentie/Wagen labo/Cambio/src/outfile.csv", b);
 		//Wegschrijven out = new Wegschrijven("C:/Users/Leen/Documents/School/outfile.csv", b);
 		//Wegschrijven out = new Wegschrijven("C:\\Users\\Birte\\OneDrive\\Documenten\\De Nayer\\Master\\semester 2\\AI Labo\\outfile.csv",b);
+		Wegschrijven out = new Wegschrijven("outfile.csv",b);
 		out.schrijfWeg();
 		
 		/*
@@ -92,7 +93,7 @@ public class Controller
 	}
 	public static void main(String[] args) 
 	{
-		System.out.println(args[0]);
+		//System.out.println(args[0]);
 		new Controller();
 
 	}
